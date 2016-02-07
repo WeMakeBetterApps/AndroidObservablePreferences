a609c9233e2b8bd3c64596df31fc75757b2bd6fa
Scott Pierce <spierce7@gmail.com>
2016-02-07 18:08:51 -0500

    Add base persistent preference tests

---
+++ android/src/test/java/com/wmba/preference/test/ImmutableObjectPersistentPreferenceTest.java
@@ -0,0 +1,26 @@
+package com.wmba.preference.test;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.wmba.preference.ObjectPersistentPreference;
+import com.wmba.preference.Preference;
+import com.wmba.preference.PreferenceSerializer;
+
+import java.lang.reflect.Type;
+
+public class ImmutableObjectPersistentPreferenceTest extends ImmutableObjectMemoryPreferenceTest {
+  @Override protected Preference<TestObject> createPreference(TestObject defaultValue) {
+    final Gson gson = new Gson();
+    PreferenceSerializer preferenceSerializer = new PreferenceSerializer() {
+      @Override public String serialize(Object value) {
+        return gson.toJson(value);
+      }
+
+      @Override public Object deserialize(String serializedValue, Type type) {
+        return gson.fromJson(serializedValue, type);
+      }
+    };
+    return new ObjectPersistentPreference<TestObject>(new MockSharedPreferences(), "test_key",
+        preferenceSerializer, new TypeToken<TestObject>() {}.getType(), defaultValue);
+  }
+}
